eb0b63a2b9a516a7e508c5ce2ee3da4e7107fa03
BorisE <e_b_s@inbox.ru>
2015-04-23 00:03:50 +0300

    0.1.5

--- ObservatoryControl/SocketServer.cs
+++ ObservatoryControl/SocketServer.cs
@@ -11,16 +11,31 @@ namespace ObservatoryCenter
 {
     public class SocketServerClass
     {
+        /// <summary>
+        /// Server parameters
+        /// </summary>
         IPAddress serverIP=IPAddress.Any;
         Int32 serverPort=1400;
 
-        public static Hashtable clientsList = new Hashtable();
-
+        /// <summary>
+        /// Main socket listener
+        /// </summary>
         Socket listenerSocket;
 
+        /// <summary>
+        /// Client list
+        /// </summary>
+        public List<ClientManager> clientsList = new List<ClientManager>();
+
+        /// <summary>
+        /// Back link to form
+        /// </summary>
         public MainForm ParentMainForm;
 
 
+        /// <summary>
+        /// Conctructor
+        /// </summary>
         public SocketServerClass(MainForm MF)
         {
             ParentMainForm=MF;
@@ -34,45 +49,31 @@ namespace ObservatoryCenter
             while (true)
             {
                 // Программа приостановлена. Ожидаем входящего соединения
-                // Это синхронное TCP приложение
                 Socket handler = listenerSocket.Accept();
                 
                 //Входящее соединение необходимо обработать
-                CreateNewClientManager(handler);
+                initClientConnection(handler);
             }
         }
 
-        public void CreateNewClientManager(Socket NewClient)
+        public void initClientConnection(Socket curSocket)
         {
-            // Буфер для входящих данных
-            byte[] bytes = new byte[1024];
+            Logging.Log("Connection from " + curSocket.RemoteEndPoint + " accepted");
 
-            //Добавляем в массив
-            clientsList.Add(clientsList.Count, NewClient);
+            //Создаем объект для обработки клиента
+            ClientManager NewClient=new ClientManager();
+            
+            //Добавляем его в список
+            clientsList.Add(NewClient);
+            
+            //Передаем ему управление
+            NewClient.CreateNewClientManager(curSocket);
 
             //Отображаем кол-во соединений в форме
             ParentMainForm.toolStripStatus_Connection.Text = "CONNECTION: " + clientsList.Count;
 
-            Logging.Log("Connection from " + NewClient.RemoteEndPoint + " accepted");
 
-            //Отправляем приветственное сообщение
-            byte[] msg = Encoding.UTF8.GetBytes("Connected to ObservatoryCenter\n\r");
-            NewClient.Send(msg);
-
-            // Получаем ответ от клиента
-            int bytesRec = NewClient.Receive(bytes);
-
-            string responseMess=Encoding.UTF8.GetString(bytes, 0, bytesRec);
-            Logging.Log("Response from сlient: " + responseMess);
-
-            // Освобождаем сокет
-            NewClient.Shutdown(SocketShutdown.Both);
-            NewClient.Close();
-
-            //Отображаем кол-во соединений в форме
-            //ParentMainForm.toolStripStatus_Connection.Text = "CONNECTION: " + clientsList.Count;
         }
-
 
         public void SocketClient(IPAddress ipAddr, Int32 port, string message)
         {
@@ -108,8 +109,92 @@ namespace ObservatoryCenter
             // Освобождаем сокет
             sender.Shutdown(SocketShutdown.Both);
             sender.Close();
-
         }
+
+    }
+
+    /// <summary>
+    /// Class for every client management
+    /// </summary>
+    public class ClientManager
+    {
+        /// <summary>
+        /// Client socket
+        /// </summary>
+        Socket ClientSocket;
+
+        /// <summary>
+        /// Client thread
+        /// </summary>
+        Thread curThread;
+
+        // Буфер для входящих данных
+        byte[] bytes = new byte[1024];
+
+        public ClientManager()
+        {
+        }
+
+        public void CreateNewClientManager(Socket NewClient)
+        {
+            this.ClientSocket = NewClient;
+            curThread = new Thread(startClientThread);
+            curThread.Start();
+        }
+
+        /// <summary>
+        /// Start unique thread for every client
+        /// </summary>
+        public void startClientThread()
+        {
+
+            //Отправляем приветственное сообщение
+            byte[] msg = Encoding.UTF8.GetBytes("Connected to ObservatoryCenter\n\r");
+            ClientSocket.Send(msg);
+
+            while (true)
+            {
+                // Получаем ответ от клиента
+                int bytesRec = ClientSocket.Receive(bytes);
+
+                string responseMess = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                Logging.Log("Message from сlient [" + ClientSocket.RemoteEndPoint + "]: " + responseMess);
+
+                string cmdMess=CommandInterpretator(responseMess);
+
+                byte[] msg2 = Encoding.UTF8.GetBytes(cmdMess + "\n\r");
+                ClientSocket.Send(msg2);
+            }
+
+
+            //Отображаем кол-во соединений в форме
+            //ParentMainForm.toolStripStatus_Connection.Text = "CONNECTION: " + clientsList.Count;
+        }
+
+        public string CommandInterpretator(string cmd)
+        {
+            string msg = "";
+            
+            switch (cmd)
+            {
+                case "TheEnd":
+                // Освобождаем сокет
+                    Logging.Log("Client [" + ClientSocket.RemoteEndPoint + "] has ended connection");
+                    ClientSocket.Shutdown(SocketShutdown.Both);
+                    ClientSocket.Close();
+                    msg = "";
+                    break;
+                default:
+                    Logging.Log("Client [" + ClientSocket.RemoteEndPoint + "]: " + "Unknown command [" + cmd + "]");
+                    msg = "Client [" + ClientSocket.RemoteEndPoint + "]: Unknown command [" + cmd + "]";
+                    break;
+            }
+
+
+            return msg;
+        }
+
+    
     
     }
 }
