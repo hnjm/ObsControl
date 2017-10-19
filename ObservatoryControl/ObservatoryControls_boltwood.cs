59eeff7ee3d40e598436340e447992b13545293a
Emchenko Boris <Emchenko Boris@DELLXPS13>
2017-10-19 14:18:27 +0300

    1.1.2

--- ObservatoryControl/ObservatoryControls_boltwood.cs
+++ ObservatoryControl/ObservatoryControls_boltwood.cs
@@ -21,7 +21,14 @@ namespace ObservatoryCenter
             BoltwoodObj_GoodState = new BoltwoodFields();
             BoltwoodObj_BadState = new BoltwoodFields();
 
+            //set settings
+            BoltwoodObj.ForcedDecimalSeparator = decimalSeparatorType.useComma;
+            BoltwoodObj.DONT_USE_DIRECT_ACCESS = false;
+
+            //load default presets
             Load_Presets();
+
+            Switch_to_GOOD();
         }
 
 
@@ -47,11 +54,11 @@ namespace ObservatoryCenter
         {
 
             //good
-            string st_good = @"{""Bolt_date"":"""",""Bolt_time"":"""",""TempUnits"":""C"",""WindSpeedUnits"":""K"",""Bolt_SkyTemp"":30,""Bolt_Temp"":10,""Bolt_SensorTemp"":10,""Bolt_WindSpeed"":2,""Bolt_Hum"":70,""Bolt_DewPoint"":-100,""Bolt_Heater"":30,""Bolt_RainFlag"":0,""Bolt_WetFlag"":0,""Bolt_SinceLastMeasure"":0,""Bolt_now"":0,""Bolt_CloudCond"":1,""Bolt_WindCond"":1,""Bolt_RainCond"":1,""Bolt_DaylighCond"":1,""Bolt_RoofCloseFlag"":0,""Bolt_AlertFlag"":0}";
+            string st_good = @"{""Bolt_date"":"""",""Bolt_time"":"""",""TempUnits"":""C"",""WindSpeedUnits"":""K"",""Bolt_SkyTemp"":30,""Bolt_Temp"":10,""Bolt_SensorTemp"":10,""Bolt_WindSpeed"":2,""Bolt_Hum"":70,""Bolt_DewPoint"":3,""Bolt_Heater"":30,""Bolt_RainFlag"":0,""Bolt_WetFlag"":0,""Bolt_SinceLastMeasure"":0,""Bolt_now"":0,""Bolt_CloudCond"":1,""Bolt_WindCond"":1,""Bolt_RainCond"":1,""Bolt_DaylighCond"":1,""Bolt_RoofCloseFlag"":0,""Bolt_AlertFlag"":0}";
             BoltwoodObj_GoodState.DeserializeFromJSON(st_good);
 
             //bad
-            string st_bad = @"{""Bolt_date"":"""",""Bolt_time"":"""",""TempUnits"":""C"",""WindSpeedUnits"":""K"",""Bolt_SkyTemp"":10,""Bolt_Temp"":10,""Bolt_SensorTemp"":10,""Bolt_WindSpeed"":20,""Bolt_Hum"":70,""Bolt_DewPoint"":-100,""Bolt_Heater"":30,""Bolt_RainFlag"":2,""Bolt_WetFlag"":2,""Bolt_SinceLastMeasure"":0,""Bolt_now"":0,""Bolt_CloudCond"":3,""Bolt_WindCond"":3,""Bolt_RainCond"":3,""Bolt_DaylighCond"":1,""Bolt_RoofCloseFlag"":1,""Bolt_AlertFlag"":1}";
+            string st_bad = @"{""Bolt_date"":"""",""Bolt_time"":"""",""TempUnits"":""C"",""WindSpeedUnits"":""K"",""Bolt_SkyTemp"":10,""Bolt_Temp"":10,""Bolt_SensorTemp"":10,""Bolt_WindSpeed"":20,""Bolt_Hum"":70,""Bolt_DewPoint"":3,""Bolt_Heater"":30,""Bolt_RainFlag"":2,""Bolt_WetFlag"":2,""Bolt_SinceLastMeasure"":0,""Bolt_now"":0,""Bolt_CloudCond"":3,""Bolt_WindCond"":3,""Bolt_RainCond"":3,""Bolt_DaylighCond"":1,""Bolt_RoofCloseFlag"":1,""Bolt_AlertFlag"":1}";
             BoltwoodObj_BadState.DeserializeFromJSON(st_bad);
         }
 
